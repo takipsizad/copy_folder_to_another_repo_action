cd3d8448e0fa708688fc1cbca808f07f9485b52f
damios <crykn@users.noreply.github.com>
2020-10-19 00:57:20 +0200

    File -> folder.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 
-if [ -z "$INPUT_SOURCE_FILE" ]
+if [ -z "$INPUT_SOURCE_FOLDER" ]
 then
-  echo "Source file must be defined"
+  echo "Source folder must be defined"
   return -1
 fi
 
@@ -23,14 +23,15 @@ git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-cp -R $INPUT_SOURCE_FILE "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+rmdir $INPUT_SOURCE_FOLDER
+cp -a $INPUT_SOURCE_FOLDER/. $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
 cd "$CLONE_DIR"
 
 echo "Adding git commit"
 git add .
 if git status | grep -q "Changes to be committed"
 then
-  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+  git commit --message "$UPDATE_MSG"
   echo "Pushing git commit"
   git push origin $INPUT_DESTINATION_BRANCH
 else
