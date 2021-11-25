d0ab8e5c33ac4fece8bb1c0c28cc1bad082cabbf
Zeljko Bekcic <zeljko.bekcic@datalbry.io>
2021-11-25 15:26:56 +0100

    fix: fixed variable expansion

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -x
 if [ -z "$INPUT_SOURCE_FOLDER" ]
 then
   echo "Source folder must be defined"
-  return -1
+  return 1
 fi
 
 if [ -z "$INPUT_DESTINATION_BRANCH" ]
@@ -27,16 +27,17 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
+if [ -n "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
   git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
   OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
 fi
 
 echo "Copying contents to git repo"
-rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
-cp -a $INPUT_SOURCE_FOLDER/. $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+# shellcheck disable=SC2115
+rm -rf "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
+mkdir -p "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
+cp -a "$INPUT_SOURCE_FOLDER/." "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
 
 echo "Adding git commit"
@@ -45,7 +46,7 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MSG"
   echo "Pushing git commit"
-  git push -u origin HEAD:$OUTPUT_BRANCH
+  git push -u origin "HEAD:$OUTPUT_BRANCH"
 else
   echo "No changes detected"
 fi
